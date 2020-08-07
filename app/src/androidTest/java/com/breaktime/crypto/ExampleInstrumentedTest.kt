8df4c59ba522fbc7513f6747195ec006bf571cd6
tellgoto <105600323+tellgoto@users.noreply.github.com>
2020-08-07 11:46:04 +0000

    project fix

--- app/src/androidTest/java/com/breaktime/crypto/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/breaktime/crypto/ExampleInstrumentedTest.kt
@@ -1,4 +1,6 @@
 package com.breaktime.crypto
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.
