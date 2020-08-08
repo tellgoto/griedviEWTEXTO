7b5ea0c3cd0701fe0bddc0f39f9084e47436de2b
tellgoto <105600323+tellgoto@users.noreply.github.com>
2020-08-08 11:07:08 +0000

    Update program.md

--- app/src/androidTest/java/com/breaktime/crypto/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/breaktime/crypto/ExampleInstrumentedTest.kt
@@ -3,4 +3,5 @@ package com.breaktime.crypto
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
-import org.junit.
+import org.junit.Test
+import org.junit.runner.R
