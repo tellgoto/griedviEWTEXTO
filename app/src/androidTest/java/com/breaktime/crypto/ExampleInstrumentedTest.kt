669f33b93f76de66513e49312a1be7200f2bf51e
tellgoto <105600323+tellgoto@users.noreply.github.com>
2020-09-06 07:08:07 +0000

    fix datetime format issue

--- app/src/androidTest/java/com/breaktime/crypto/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/breaktime/crypto/ExampleInstrumentedTest.kt
@@ -9,4 +9,6 @@ import org.junit.runner.RunWith
 import org.junit.Assert.*
 
 /**
- * Instrumented test, which will execute on 
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [t
