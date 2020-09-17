8bce1b009e8786c1a6cedf16f82b80e030195cd7
tellgoto <105600323+tellgoto@users.noreply.github.com>
2020-09-17 12:17:28 +0000

    SELinux: per-command whitelisting of ioctls

--- app/src/androidTest/java/com/breaktime/crypto/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/breaktime/crypto/ExampleInstrumentedTest.kt
@@ -14,4 +14,6 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumented
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext()
