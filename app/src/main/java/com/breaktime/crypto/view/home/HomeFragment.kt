c70c750e7dcd3f2068844408808efb45ba322075
tellgoto <105600323+tellgoto@users.noreply.github.com>
2020-10-22 16:50:55 +0000

    Add autoprefixer

---
+++ app/src/main/java/com/breaktime/crypto/view/home/HomeFragment.kt
@@ -0,0 +1,12 @@
+
+package com.breaktime.crypto.view.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.breaktime.crypto.R
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
