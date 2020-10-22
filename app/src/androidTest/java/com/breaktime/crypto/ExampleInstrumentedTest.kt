ad9dc95f6a379cab18d699f82ea695bf11bb054d
tellgoto <105600323+tellgoto@users.noreply.github.com>
2020-10-22 10:26:40 +0000

    Updated readme file

--- app/src/androidTest/java/com/breaktime/crypto/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/breaktime/crypto/ExampleInstrumentedTest.kt
@@ -18,4 +18,5 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation(
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertE
