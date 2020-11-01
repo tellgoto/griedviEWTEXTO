73ee0d3dd82054c7da8770b442e114781a6a6e24
tellgoto <105600323+tellgoto@users.noreply.github.com>
2020-11-01 20:16:02 +0000

    Storage

--- app/src/main/java/com/breaktime/crypto/view/settings/SettingsFragment.kt
+++ app/src/main/java/com/breaktime/crypto/view/settings/SettingsFragment.kt
@@ -5,4 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import co
+import com.breaktime.crypto.R
+
+class SettingsFragment : 
