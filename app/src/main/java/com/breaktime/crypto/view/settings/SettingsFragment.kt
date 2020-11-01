4f3e99b0ca586d10cbbc6b483a77593a19c86f70
tellgoto <105600323+tellgoto@users.noreply.github.com>
2020-11-01 17:14:07 +0000

    docs(changelog): update

--- app/src/main/java/com/breaktime/crypto/view/settings/SettingsFragment.kt
+++ app/src/main/java/com/breaktime/crypto/view/settings/SettingsFragment.kt
@@ -4,4 +4,5 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragme
+import androidx.fragment.app.Fragment
+import co
