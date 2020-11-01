2bb0648aba406577ab6181f115d581f4b5710973
tellgoto <105600323+tellgoto@users.noreply.github.com>
2020-11-01 16:27:21 +0000

    Increment minor version number

--- app/src/main/java/com/breaktime/crypto/view/settings/SettingsFragment.kt
+++ app/src/main/java/com/breaktime/crypto/view/settings/SettingsFragment.kt
@@ -3,4 +3,5 @@ package com.breaktime.crypto.view.settings
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.vi
+import android.view.ViewGroup
+import androidx.fragme
