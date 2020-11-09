6b3824843fc09c7043f6380db0bc0e996e671d32
tellgoto <105600323+tellgoto@users.noreply.github.com>
2020-11-09 20:05:03 +0000

    0.13

--- app/src/main/java/com/breaktime/crypto/view/splash/SplashFragment.kt
+++ app/src/main/java/com/breaktime/crypto/view/splash/SplashFragment.kt
@@ -3,4 +3,5 @@ package com.breaktime.crypto.view.splash
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGr
+import android.view.ViewGroup
+import androidx.fragment.app.Frag
