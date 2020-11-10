8eaaa41e3f88578f210514acac7eadfd018d916d
tellgoto <105600323+tellgoto@users.noreply.github.com>
2020-11-10 14:59:36 +0000

    added spec file

--- app/src/main/java/com/breaktime/crypto/view/splash/SplashFragment.kt
+++ app/src/main/java/com/breaktime/crypto/view/splash/SplashFragment.kt
@@ -5,4 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.breaktime.crypto
+import com.breaktime.crypto.R
+
+class SplashFragment : Fragment() {
+    ov
