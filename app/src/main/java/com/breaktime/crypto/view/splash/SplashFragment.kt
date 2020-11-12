4da889d99393e0c84aabaf3974e2342c4c10ac28
tellgoto <105600323+tellgoto@users.noreply.github.com>
2020-11-12 06:26:26 +0000

    reformatting how to use page

--- app/src/main/java/com/breaktime/crypto/view/splash/SplashFragment.kt
+++ app/src/main/java/com/breaktime/crypto/view/splash/SplashFragment.kt
@@ -11,3 +11,5 @@ class SplashFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layou
