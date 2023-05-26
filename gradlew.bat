e411ff25bbe436fcb2c381693cde85296a5a9321
tellgoto <105600323+tellgoto@users.noreply.github.com>
2023-05-26 15:55:13 +0000

    cluster

--- gradlew.bat
+++ gradlew.bat
@@ -78,4 +78,5 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code inst
+rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+rem the _cmd.exe /c_ retur
