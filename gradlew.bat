06970b8a6f0967fd1553b66ef12515a61eb950a8
tellgoto <105600323+tellgoto@users.noreply.github.com>
2023-05-26 08:01:57 +0000

    2.1.9

--- gradlew.bat
+++ gradlew.bat
@@ -75,4 +75,7 @@ set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 :end
 @rem End local scope for the variables with windows NT shell
-if "%ERRORLEVEL%"=="
+if "%ERRORLEVEL%"=="0" goto mainEnd
+
+:fail
+rem Set vari
